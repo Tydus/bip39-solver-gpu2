99857f72f17c72f50efd72175f24956b3bcc2362
Tydus <Tydus@Tydus.org>
2021-02-26 02:49:15 +0000

    Add check.py

---
+++ check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import base58
+
+from py_crypto_hd_wallet import HdWalletFactory, HdWalletCoins, HdWalletSpecs
+
+hd_wallet_fact = HdWalletFactory(HdWalletCoins.BITCOIN)
+
+def mnemonic_to_addr(mnemonic):
+    wallet = hd_wallet_fact.CreateFromMnemonic("my_wallet_name", mnemonic)
+    wallet.Generate()
+    return wallet.ToDict()['addresses']['address_1']['address']
+
+if __name__ == "__main__":
+    while True:
+        a = input()
+        if not a: break
+        prit(mnemonic_to_addr(a))
