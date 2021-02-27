7862b81c303e344e3b9f8c3b12aaf1d82cd0cbf3
Tydus <Tydus@Tydus.org>
2021-02-27 14:34:00 +0000

    Support password in check.py

--- check.py
+++ check.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
 import base58
+import getpass
 
 from py_crypto_hd_wallet import HdWalletFactory, HdWalletCoins, HdWalletSpecs
 
 hd_wallet_fact = HdWalletFactory(HdWalletCoins.BITCOIN)
 
-def mnemonic_to_addr(mnemonic):
-    wallet = hd_wallet_fact.CreateFromMnemonic("my_wallet_name", mnemonic)
+def mnemonic_to_addr(mnemonic, passphrase=''):
+    wallet = hd_wallet_fact.CreateFromMnemonic("my_wallet_name", mnemonic, passphrase=passphrase)
     wallet.Generate()
     return wallet.ToDict()['addresses']['address_1']['address']
 
 if __name__ == "__main__":
+    passphrase = getpass.getpass()
+
     while True:
         a = input()
         if not a: break
